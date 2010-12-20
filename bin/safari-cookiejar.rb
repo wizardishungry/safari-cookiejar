610713d22773c62a39c9ec3226b6fe9a00922da0
Jon Williams <jonathan.williams@gmail.com>
2010-12-20 02:17:57 -0500

    boilerplate commandline stuff

--- bin/safari-cookiejar.rb
+++ bin/safari-cookiejar.rb
@@ -1,1 +1,21 @@
 #!/usr/bin/env ruby
+
+
+class SafariCookiejar
+    def usage
+        puts "#{$0} [curl|wget] arguments";
+    end
+    def initialize(argv)
+        @argv = argv
+    end
+    def run
+    end
+end
+
+jar = SafariCookiejar.new ARGV
+
+if ARGV.empty?
+    jar.usage
+else
+    jar.run
+end
